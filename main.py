db82d8f6353450f19fc23e433243a9fb0934bbf5
Egor Matveev <emmtvv@icloud.com>
2020-04-04 14:34:56 +0300

    buttons added

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from telegram.ext import Updater, CallbackQueryHandler
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from json import loads
-
+from telegram import ReplyKeyboardMarkup
 
 token = '1188178965:AAHr8WXr5AsFRS_v62IDHnWi_3kDj5oN0oc'
 
 data = loads(open('past_matches.json', 'r').read())
+
+users = {}
+
+games = set([i['game'] for i in data] + ['CS GO'])
+
+keyboard = ReplyKeyboardMarkup([[i] for i in games], True)
 
 
 def key(x):
@@ -15,15 +21,28 @@ def key(x):
 
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id, "Привет! Спроси меня о результатах матчей!")
+    context.bot.send_message(chat_id, "Привет! Спроси меня о результатах матчей!", reply_markup=keyboard)
 
 
 def answer(update, context):
     chat_id = update.effective_chat.id
     text = update.message.text
-    selected = sorted([i for i in data if i['first_team'].replace(' ', '') == text.replace(' ', '') or i['second_team'].replace(' ', '') == text.replace(' ', '')], key=key)
+
+    if text == 'Все игры':
+        users.pop(chat_id)
+        context.bot.send_message(chat_id, 'Включен фильтр: Все игры', reply_markup=keyboard)
+        return
+    if text in games:
+        users[chat_id] = text
+        context.bot.send_message(chat_id, 'Включен фильтр: ' + text, reply_markup=keyboard)
+        return
+
+    new_data = data
+    if chat_id in users:
+        new_data = [i for i in data if i['game'] == users[chat_id]]
+    selected = sorted([i for i in new_data if i['first_team'].replace(' ', '') == text.replace(' ', '') or i['second_team'].replace(' ', '') == text.replace(' ', '')], key=key)
     if not selected:
-        context.bot.send_message(chat_id, "Такой команды не найдено")
+        context.bot.send_message(chat_id, "Такой команды не найдено", reply_markup=keyboard)
     else:
         n = min(3, len(selected))
         mes = ''
@@ -35,7 +54,7 @@ def answer(update, context):
             else:
                 mes += cur['date_real'] + ' | ' + cur['first_team'] + ' | <u><i><b>' + \
                        cur['second_team'] + '</b></i></u> | ' + cur['id'] + '\n'
-        context.bot.send_message(chat_id, mes, parse_mode='html')
+        context.bot.send_message(chat_id, mes, parse_mode='html', reply_markup=keyboard)
 
 
 if __name__ == '__main__':
